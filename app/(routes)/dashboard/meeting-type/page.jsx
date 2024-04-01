a0bcf3ef06a27277aa1914f5138f3f6dc9031d79
GGARJUN <techarjung@gmail.com>
2024-04-01 17:15:33 +0530

    All fix Complete

--- app/(routes)/dashboard/meeting-type/page.jsx
+++ app/(routes)/dashboard/meeting-type/page.jsx
@@ -2,6 +2,7 @@
 import { Input } from '@/components/ui/input'
 
 import MeetingEventList from './_components/MeetingEventList'
+import SideNavBar from '../_components/SideNavBar'
 
 
 function MeetingType() {
@@ -9,6 +10,9 @@ function MeetingType() {
  
   return (
     <div className='p-5'>
+      <div className='md:hidden  w-full  '>
+      <SideNavBar/>
+      </div>
       <div className='flex flex-col gap-5'>
         <h2 className='font-bold text-3xl'>Meeting Event Type</h2>
         <Input placeholder="Search" className="max-w-xs " />
